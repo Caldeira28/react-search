c2e64005c9d6f0542fb3e56a444d3ba2f8b77276
Caldeira28 <diogo.caldeira@hotmail.com>
2022-08-09 22:18:23 +0100

    search

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react";
 import BasicCard from "./components/BasicCard";
-import { Rings } from  'react-loader-spinner'
+import { Rings } from "react-loader-spinner";
 import "./index.css";
 
 export default function App() {
     const [data, setData] = useState();
     const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    const [url, setUrl] = useState("https://dev.to/api/articles");
+    
+    let [inputValue, setInputValue] = useState("");
+    let [information, setInformation] = useState("");
 
     useEffect(() => {
-        let url = "https://dev.to/api/articles";
+        
         function getArticles() {
+          setLoading(true);
             fetch(url)
                 //This operation returns a promise that could either resolve or reject
                 // we must resolve the Response object to JSON format using the json() method
@@ -19,19 +23,14 @@ export default function App() {
                 .then((articles) => {
                     console.log(articles);
                     setData(articles);
-                    setError(null);
                 })
-                // In case the promise rejects, we will handle the error using the catch()
-                .catch((err) => {
-                    setError(err.message);
-                    setData(null);
-                })
+
                 .finally(() => {
                     setLoading(false);
                 });
         }
         getArticles();
-    }, []);
+    }, [url]);
 
     const articlesArray = data?.map((article) => (
         <BasicCard
@@ -46,23 +45,46 @@ export default function App() {
         />
     ));
 
-    if (!loading && !error) {
+    function updateInputValue(event) {
+        console.log(event.target.value);
+        setInputValue(event.target.value);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log("dentro do submit");
+        if (inputValue) {
+          setUrl(`https://dev.to/api/articles?tag=${inputValue}`);
+            setInformation(`Find all about ${inputValue}`);
+        } else {
+            setInformation("Please search for an article topic");
+        }
+    }
+
+    if (!loading) {
         return (
             <div className="App">
                 <h1>Articles</h1>
+                <div>
+                    <form onSubmit={handleSubmit}>
+                        <input type="search" onChange={updateInputValue} />
+                        <button type="submit">Search</button>
+                    </form>
+                    <h3>Search for {inputValue}</h3>
+                    <h4>{information}</h4>
+                </div>
                 <div className="articles">{data && articlesArray}</div>
             </div>
         );
-    } else if (error) {
-        return <div>There is a problem fetching the post data - {error}</div>;
     } else {
-        return (<Rings
-          height = "80"
-          width = "80"
-          radius = "9"
-          color = 'green'
-          ariaLabel = 'three-dots-loading'     
-          
-        />);
+        return (
+            <Rings
+                height="80"
+                width="80"
+                radius="9"
+                color="green"
+                ariaLabel="three-dots-loading"
+            />
+        );
     }
 }
